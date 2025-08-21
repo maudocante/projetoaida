4239631ded36449976993e2129ddf4f2083ad618
maudocante <ummominhafilha@gmail.com>
2025-08-21 01:11:38 +0000

    Update criar_contas_nextcloud.sh
    Adiciondo display-name e grupo

--- criar_contas_nextcloud.sh
+++ criar_contas_nextcloud.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker-compose exec -T app php occ group:add --display-name GU GU
+docker-compose exec -T app php occ group:add --display-name "Grupo de trabalho" "GU"
 # Cria o usuário 'ana' no Nextcloud via docker-compose com senha definida
-OC_PASS=12345678 docker-compose exec -T app php occ user:add --password-from-env ana
+docker-compose exec -T app php occ user:add --display-name "Ana" "ana"
 
