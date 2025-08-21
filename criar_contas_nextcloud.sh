5ff2f6dea2820b2b3f308ef4ec0304743ba829b3
maudocante <ummominhafilha@gmail.com>
2025-08-21 16:56:36 +0000

    Adicionado -u www-data ao file criar...

--- criar_contas_nextcloud.sh
+++ criar_contas_nextcloud.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-docker-compose exec -T app php occ group:add --display-name "Grupo de trabalho" "GU"
+docker-compose -u www-data exec -T app php occ group:add --display-name "Grupo de trabalho" "GU"
 # Cria o usuário 'ana' no Nextcloud via docker-compose com senha definida
-docker-compose exec -T app php occ user:add --display-name "Suleimane" --group "GU" suleimane
+docker-compose -u www-data exec -T app php occ user:add --display-name "Ana" --group "GU" ana
+# Cria o usuário 'suleimane' no Nextcloud via docker-compose com senha definida
+docker-compose -u www-data exec -T app php occ user:add --display-name "Suleimane" --group "GU" suleimane
