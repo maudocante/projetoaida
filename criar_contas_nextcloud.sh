7e3e462561d2844949615dbd18a2375e41bca37a
maudocante <ummominhafilha@gmail.com>
2025-08-22 15:27:41 +0000

    Generalização do script

--- criar_contas_nextcloud.sh
+++ criar_contas_nextcloud.sh
@@ -3,44 +3,34 @@
 GRUPO="GU"
 GRUPO_DISPLAY="Grupo de trabalho"
 
-USUARIO_ANA="ana"
-ANA_DISPLAY="Ana"
-ANA_SENHA="Ana11223344"
+USUARIO="ana"
+DISPLAY="Ana"
+SENHA="Ana11223344"
 
-USUARIO_SULEIMANE="suleimane"
-SULEIMANE_DISPLAY="Suleimane"
-SULEIMANE_SENHA="Suleimane1122"
 
 criar_grupo() {
   # Cria o grupo com display name
   docker-compose exec -u www-data -T app php occ group:add --display-name "$GRUPO_DISPLAY" "$GRUPO"
 }
 
-criar_usuario_ana() {
+criar_usuario() {
   # Cria o usuário Ana no grupo com senha definida
-  docker-compose exec -u www-data -T -e OC_PASS="$ANA_SENHA" app php occ user:add --display-name "$ANA_DISPLAY" --group "$GRUPO" --password-from-env "$USUARIO_ANA"
+  docker-compose exec -u www-data -T -e OC_PASS="$SENHA" app php occ user:add --display-name "$DISPLAY" --group "$GRUPO" --password-from-env "$USUARIO"
 }
 
-criar_usuario_suleimane() {
-  # Cria o usuário Suleimane no grupo com senha definida
-  docker-compose exec -u www-data -T -e OC_PASS="$SULEIMANE_SENHA" app php occ user:add --display-name "$SULEIMANE_DISPLAY" --group "$GRUPO" --password-from-env "$USUARIO_SULEIMANE"
-}
 
 echo "Escolha uma opção:"
 echo "1) Criar grupo $GRUPO"
-echo "2) Criar usuário $ANA_DISPLAY"
-echo "3) Criar usuário $SULEIMANE_DISPLAY"
+echo "2) Criar usuário $DISPLAY"
 echo "4) Criar todos"
 read -r -p "Opção: " opcao
 
 case "$opcao" in
   1) criar_grupo ;;
-  2) criar_usuario_ana ;;
-  3) criar_usuario_suleimane ;;
+  2) criar_usuario ;;
   4)
     criar_grupo
-    criar_usuario_ana
-    criar_usuario_suleimane
+    criar_usuario
     ;;
   *) echo "Opção inválida." ;;
 esac
